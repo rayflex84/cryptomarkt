74d2d93950ac1175c0e98170dbc1ffacf65cf782
rayflex84 <45134226+rayflex84@users.noreply.github.com>
2021-01-19 02:03:28 +0000

    Update index.php

--- index.php
+++ index.php
@@ -5,5 +5,13 @@ $bot_token = '1412102282:AAES2QSyW4iUCkHN0UcTgNd3Uj-kyDIrc8M';
 $telegram = new Telegram($bot_token);
 $text = $telegram->Text();
 $chat_id = $telegram->ChatID();
-$content = array('chat_id' => $chat_id, 'text' => $text);
+
+if ($text == '/start') {
+    $option = [["\xF0\x9F\x90\xAE"], ['Git', 'Credit']];
+    // Create a permanent custom keyboard
+    $keyb = $telegram->buildKeyBoard($option, $onetime = false);
+    $content = ['chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => "Welcome to CowBot \xF0\x9F\x90\xAE \nPlease type /cowsay or click the Cow button !"];
+} else {
+    $content = array('chat_id' => $chat_id, 'text' => $text);
+}
 $telegram->sendMessage($content);
