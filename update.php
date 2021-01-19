29cc0d181d78903ecdb0e553d46a666e9908d1c2
rayflex84 <45134226+rayflex84@users.noreply.github.com>
2021-01-19 01:12:20 +0000

    Update update.php

--- update.php
+++ update.php
@@ -1,4 +1,10 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Methods: GET,POST,OPTIONS,DELETE,PUT");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
 include 'Telegram.php';
 $bot_token = '1412102282:AAES2QSyW4iUCkHN0UcTgNd3Uj-kyDIrc8M';
 $telegram = new Telegram($bot_token);
