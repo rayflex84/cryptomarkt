b7fe0e68b79ffb0f8f16fc14c9c9990a66f17ffd
rayflex84 <45134226+rayflex84@users.noreply.github.com>
2021-01-19 02:09:57 +0000

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ if ($text == '/start') {
     $option = [["\xF0\x9F\x90\xAE"], ['Git', 'Credit']];
     // Create a permanent custom keyboard
     $keyb = $telegram->buildKeyBoard($option, $onetime = false);
-    $content = ['chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => "Welcome to CowBot \xF0\x9F\x90\xAE \nPlease type /cowsay or click the Cow button !"];
+    $content = ['chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => "Welcome ".$telegram->FirstName()." to CowBot \xF0\x9F\x90\xAE \nPlease type /cowsay or click the Cow button !"];
 } else {
     $content = array('chat_id' => $chat_id, 'text' => $text);
 }
